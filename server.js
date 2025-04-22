3aa60308238cbb73ff5e7eb8fcc25e70b24b3952
ahpu9158 <titiporn02468@gmail.com>
2025-04-22 10:10:59 +0700

    feat: initialize project with Express server and essential middleware
    - Added package.json with dependencies and devDependencies
    - Created server.js to set up Express server
    - Implemented middleware for security (helmet, mongo-sanitize, xss, hpp)
    - Configured rate limiting and CORS
    - Set up Swagger for API documentation
    - Established connection to MongoDB

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const {xss} = require('express-xss-sanitizer');
+const hpp = require('hpp');
+const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+const nodemailer = require('nodemailer')
+
+// Load env vars
+dotenv.config({path: './config/config.env'});
+
+connectDB();
+
+// Route files
+const hotels = require ('./routes/hotels');
+const bookings = require('./routes/bookings');
+const reviews = require('./routes/reviews');
+const rooms = require('./routes/rooms');
+const payments = require('./routes/payments');
+const auth = require('./routes/auth');
+const rateLimit = require('express-rate-limit');
+    
+const app=express();
+
+//Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, //10 mins
+    max: 100000
+});
+
+//Enable CORS
+app.use(cors());
+
+// Body parser
+app.use(express.json());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(limiter);
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+            styleSrc: ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+            imgSrc: ["'self'", 'data:', 'cdn.jsdelivr.net'],
+            },
+        },
+    })
+);
+
+app.use(hpp());
+// Cookie parser
+app.use(cookieParser());
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+const PORT=process.env.PORT || 5000;
+
+process.on('unhandledRejection', (err,promise)=>{
+    console.log(`Error: ${err.message}`); 
+    //Close server & exit process 
+    server.close(()=>process.exit(1));
+});
+
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  }
+  
+  module.exports = app;
